1fd6d55bfc332c19bae5fa21da70e2abba38bf09
Andrew <github@andrewmarrone.com>
2016-07-28 10:48:22 -0700

    Removed Blog functionality
    Removed blog functionality to maintain focus on the 2FA portion only.
    Removed username checks as well.

--- flaskr.py
+++ flaskr.py
@@ -1,15 +1,11 @@
-import sqlite3
 import ConfigParser
 import duo_web as duo
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 # config
-DATABASE = '/tmp/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'dev key'
-USERNAME = ['andrew', 'testuser']
-PASSWORD = 'default'
 
 
 # create flask application
@@ -36,47 +32,10 @@ def app_config(filename='app.conf'):
     return config.get('app', 'skey')
 
 
-# make a database connection
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-# Request DB connections helpers
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
-
-
 # Routing functions
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
-
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
-    g.db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    return render_template('show_entries.html')
 
 
 @app.route('/mfa', methods=['GET', 'POST'])
@@ -100,8 +59,8 @@ def success():
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] not in USERNAME:
-            error = 'Invalid Username'
+        if request.form['username'] == "":
+            error = 'Invalid Username. Please type something.'
         else:
             session['logged_in'] = True
             session['user'] = request.form['username']
